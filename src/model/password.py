f378eda7a8d60a86d2a31348c96d7d0c60751f01
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-27 23:53:55 -0300

    hotfix: pydantic problem optional values and json response

--- src/model/password.py
+++ src/model/password.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Password(BaseModel):
-    numbers: bool = None
-    low_case: bool = None
-    up_case: bool = None
-    special_char_1: bool = None
-    special_char_2: bool = None
+    numbers: Optional[bool] = None
+    low_case: Optional[bool] = None
+    up_case: Optional[bool] = None
+    special_char_1: Optional[bool] = None
+    special_char_2: Optional[bool] = None
     length: int = 0
