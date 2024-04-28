58eae94d2dc73ffe8698cbac5103c0d5ac4fa598
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-27 22:59:09 -0300

    feat: controller fixed to link to service

--- src/controller/controller_password.py
+++ src/controller/controller_password.py
@@ -1,3 +1,5 @@
+from fastapi import Response
+
 from src.model.password import Password
 from src.service.service_password import ServicePassword
 
@@ -8,5 +10,5 @@ class ControllerPassword:
     def __init__(self):
         self._service = ServicePassword()
 
-    async def generate_password(self, password: Password) -> str | None:
+    async def generate_password(self, password: Password) -> Response | None:
         return await self._service.generate_password(password=password)
