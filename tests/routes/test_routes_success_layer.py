f652b06bbd51c3e30b65b3879b722a595e824995
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 14:49:18 -0300

    test: case success one

--- tests/routes/test_routes_success_layer.py
+++ tests/routes/test_routes_success_layer.py
@@ -16,3 +16,14 @@ from src.app.app import app
 
 URL = "/api/generate/password"
 API_URL_BASE = environ["API_URL_BASE_TEST"]
+
+
+@pytest.mark.asyncio
+async def test_generate_password_success_one() -> None:
+    async with AsyncClient(app=app, base_url=API_URL_BASE) as client:
+        response = await client.post(
+            url=URL,
+            json=password_one
+        )
+
+        assert response.status_code == 201
