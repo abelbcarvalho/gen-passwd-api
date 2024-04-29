a00cd6f38c771110922c6812cc5515d4a037d4a8
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 16:47:05 -0300

    test: service success case tests layer done

--- tests/service/test_service_success_layer.py
+++ tests/service/test_service_success_layer.py
@@ -1,7 +1,5 @@
 import pytest
 
-from src.model.password import Password
-
 from tests.mocks.passwords import (
     password_one,
     password_two,
@@ -16,16 +14,32 @@ from tests.service.generate_password_service import generate_password
 class TestServiceSuccessLayer:
     @pytest.mark.asyncio
     async def test_password_service_success_one(self) -> None:
-        pass_one = Password.parse_obj(password_one)
+        new_password = await generate_password(password_one)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == password_one["length"]
 
     @pytest.mark.asyncio
     async def test_password_service_success_two(self) -> None:
-        pass_two = Password.parse_obj(password_two)
+        new_password = await generate_password(password_two)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == password_two["length"]
 
     @pytest.mark.asyncio
     async def test_password_service_success_three(self) -> None:
-        pass_three = Password.parse_obj(password_three)
+        new_password = await generate_password(password_three)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == password_three["length"]
 
     @pytest.mark.asyncio
     async def test_password_service_success_four(self) -> None:
-        pass_four = Password.parse_obj(password_four)
+        new_password = await generate_password(password_four)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == password_four["length"]
