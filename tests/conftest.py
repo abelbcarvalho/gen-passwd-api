f01e0b5572c4e6df5f188941b1857aee3b47ae17
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 11:47:39 -0300

    chore: requirements txt updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,11 @@
 import pytest
 
-from typing import Generator
-
 from fastapi.testclient import TestClient
 
 from src.app.app import app
 
 
 @pytest.fixture(scope="function")
-async def client() -> Generator:
+async def client() -> TestClient:
     with TestClient(app) as test:
         yield test
