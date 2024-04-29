e61073b52ddd92ce8300cdfd9280121102dca7f7
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 17:02:15 -0300

    tests: success generate case methods pre build

--- tests/generate/test_generate_success_layer.py
+++ tests/generate/test_generate_success_layer.py
@@ -5,13 +5,29 @@ from src.model.password import Password
 from src.generate.generate import Generate
 
 from tests.mocks.passwords import (
-    password_error_one,
-    password_error_two,
-    password_error_three,
-    password_error_four
+    password_one,
+    password_two,
+    password_three,
+    password_four
 )
 
 
 @pytest.mark.asyncio(scope="class")
 class TestGenerateSuccessful:
     generate: Generate = Generate()
+
+    @pytest.mark.asyncio
+    async def test_password_generate_success_one(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_password_generate_success_two(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_password_generate_success_three(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_password_generate_success_four(self) -> None:
+        pass
