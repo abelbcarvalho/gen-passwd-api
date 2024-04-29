2d0f2b9d859cc8a2284b184736193621053bf7f5
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 15:21:00 -0300

    tests: class and methods assing

--- tests/routes/test_routes_errors_layer.py
+++ tests/routes/test_routes_errors_layer.py
@@ -16,3 +16,22 @@ from src.app.app import app
 
 URL = "/api/generate/password"
 API_URL_BASE = environ["API_URL_BASE_TEST"]
+
+
+@pytest.mark.asyncio(scope="class")
+class TestRoutesErrorsLayer:
+    @pytest.mark.asyncio
+    async def test_routes_errors_layer_one(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_routes_errors_layer_two(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_routes_errors_layer_three(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_routes_errors_layer_four(self) -> None:
+        pass
