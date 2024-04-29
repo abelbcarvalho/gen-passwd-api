0762b10428a213d8ea78e3ce358e5475435190ec
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 15:48:38 -0300

    test: methods assing created

--- tests/controller/test_controller_success_layer.py
+++ tests/controller/test_controller_success_layer.py
@@ -16,3 +16,19 @@ class TestControllerSuccessLayer:
     _pass_two: Password = Password.parse_obj(password_two)
     _pass_three: Password = Password.parse_obj(password_three)
     _pass_four: Password = Password.parse_obj(password_four)
+
+    @pytest.mark.asyncio
+    async def test_controller_password_success_one(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_controller_password_success_two(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_controller_password_success_three(self) -> None:
+        pass
+
+    @pytest.mark.asyncio
+    async def test_controller_password_success_four(self) -> None:
+        pass
