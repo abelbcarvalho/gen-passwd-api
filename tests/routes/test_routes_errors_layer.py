1943a1816a593dedbb9440fdb0d0190e7db9d7c5
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 15:19:12 -0300

    feat: module for error layer ready

---
+++ tests/routes/test_routes_errors_layer.py
@@ -0,0 +1,18 @@
+import pytest
+
+from os import environ
+
+from httpx import AsyncClient
+
+from tests.mocks.passwords import (
+    password_error_one,
+    password_error_two,
+    password_error_three,
+    password_error_four
+)
+
+from src.app.app import app
+
+
+URL = "/api/generate/password"
+API_URL_BASE = environ["API_URL_BASE_TEST"]
