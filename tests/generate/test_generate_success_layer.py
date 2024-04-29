e56dc1b3c2e4b4bd4ea20e55740f8cabb89c9795
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 17:11:03 -0300

    tests: generate success layer ready

--- tests/generate/test_generate_success_layer.py
+++ tests/generate/test_generate_success_layer.py
@@ -18,16 +18,40 @@ class TestGenerateSuccessful:
 
     @pytest.mark.asyncio
     async def test_password_generate_success_one(self) -> None:
-        pass
+        pass_one = Password.parse_obj(password_one)
+
+        new_password = await self.generate.generate_password(pass_one)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == pass_one.length
 
     @pytest.mark.asyncio
     async def test_password_generate_success_two(self) -> None:
-        pass
+        pass_two = Password.parse_obj(password_two)
+
+        new_password = await self.generate.generate_password(pass_two)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == pass_two.length
 
     @pytest.mark.asyncio
     async def test_password_generate_success_three(self) -> None:
-        pass
+        pass_three = Password.parse_obj(password_three)
+
+        new_password = await self.generate.generate_password(pass_three)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == pass_three.length
 
     @pytest.mark.asyncio
     async def test_password_generate_success_four(self) -> None:
-        pass
+        pass_four = Password.parse_obj(password_four)
+
+        new_password = await self.generate.generate_password(pass_four)
+
+        assert new_password is not None
+        assert type(new_password) is str
+        assert len(new_password) == pass_four.length
