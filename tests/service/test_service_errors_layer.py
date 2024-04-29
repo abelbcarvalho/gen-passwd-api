de8707ba2b1f5b2fd48dfbbadc39b4964b6bf7f6
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-29 16:39:53 -0300

    test: layer error service methods assign

--- tests/service/test_service_errors_layer.py
+++ tests/service/test_service_errors_layer.py
@@ -16,4 +16,18 @@ from tests.service.generate_password_service import generate_password
 
 @pytest.mark.asyncio(scope="class")
 class TestServiceErrorsLayer:
-    pass
+    @pytest.mark.asyncio
+    async def test_password_error_one(self) -> None:
+        pass_one = Password.parse_obj(password_error_one)
+
+    @pytest.mark.asyncio
+    async def test_password_error_two(self) -> None:
+        pass_two = Password.parse_obj(password_error_two)
+
+    @pytest.mark.asyncio
+    async def test_password_error_three(self) -> None:
+        pass_three = Password.parse_obj(password_error_three)
+
+    @pytest.mark.asyncio
+    async def test_password_error_four(self) -> None:
+        pass_four = Password.parse_obj(password_error_four)
